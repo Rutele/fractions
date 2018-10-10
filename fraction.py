990a7874ea5ada30316856aade471aad4ddcadaf
Michał Ruciński <rucinskimichal7@gmail.com>
2018-10-10 15:08:57 +0200

    Fraction Class. Passes all of the tests.

--- fraction.py
+++ fraction.py
@@ -1,32 +1,49 @@
-"""
-This file contains definition of a fraction class.
-
-You should put complete class here. It must be named `Fragion` and must have the following properties:
-
-- four basic mathematical operators defined;
-- elegant conversion to string in the form '3/2';
-- simplification and clean-up on construction: both attribute divided by the greatest common divisor
-  sign in the nominator, denominator not zero (ValueError should be raised in such case), both attributes
-  must be integers (ValueError if not),
-- method `value` returning float value of the fraction.
-"""
 from math import gcd
 
-
 class Fraction(object):
-    """
-    Fraction class.
-    """
-
+    
+    nom = None
+    denom = None
+    
     def __init__(self, nom, denom):
-        self.nom = nom
+        self.nom = nom 
         self.denom = denom
+        
+        if self.denom == 0:
+            raise ValueError("Denominator must not be zero.")
+        
+        divisor = gcd(self.nom, self.denom)
+        self.nom = int(nom/divisor)
+        self.denom = int(denom/divisor)
+        
+        if (self.denom < 0):
+            self.denom = -1 * self.denom
+            self.nom = -1 * self.nom
 
     def __str__(self):
-        return f"{self.nom}/{self.denom}"
+        if self.denom == 1:
+            return "{0.nom}".format(self)
+        else:
+            return "{0.nom}/{0.denom}".format(self)
+    
+    def __mul__(self, number):
+        return Fraction(self.nom*number.nom, self.denom*number.denom)
+    
+    def __truediv__(self, number):
+        return Fraction(self.nom*number.denom, self.denom*number.nom)
+    
+    def __add__(self, number):
+        n1 = self.nom * number.denom 
+        n2 = self.denom * number.nom 
+        return Fraction(n1+n2, self.denom*number.denom)
+    
+    def __sub__(self, number):
+        n1 = self.nom * number.denom 
+        n2 = self.denom * number.nom 
+        return Fraction(n1-n2, self.denom*number.denom)
 
-    def __repr__(self):
-        return f"Fraction({self.nom}, {self.denom})"
+    def __eq__(self, number):
+        return self.nom == number.nom and self.denom == number.denom
 
-    def __eq__(self, other):
-        return self.nom == other.nom and self.denom == other.denom
+    def value(self):
+        return self.nom/self.denom
